1d5326476ecfe1b526939d389f6776b5dfc360e2
Anmolpreet <anmolpreet_sandhu@hotmail.com>
2015-10-11 21:45:58 -0400

    2nd commit

--- src/deleteDups/DeleteDups.java
+++ src/deleteDups/DeleteDups.java
@@ -11,6 +11,8 @@ public class DeleteDups {
 	
 	    public static void main(String[] args) {
 
+	    	
+	    	//creating a static linked list for example purpose, You can create dynamic if you want.
 	        LinkedList<Integer> list = new LinkedList<Integer>();
 	        list.addLast(1);
 	        list.addLast(2);
@@ -20,23 +22,19 @@ public class DeleteDups {
 	        list.addLast(4);
 	        list.addLast(4);
 	        
-	        System.out.println(list);
+	        System.out.println(list); //before deleting duplicates
 	        deleteDups(list);
-	        System.out.println(list);
+	        System.out.println(list); // after deleting duplicates
 	    }
 
 	    public static void deleteDups(LinkedList<Integer> list) {
 	        HashSet<Integer> s = new HashSet<Integer>();
-	        s.addAll(list);
-	        
-	        
-	        //System.out.println();
+	        s.addAll(list); //adding all list elements to hashset.
+	      
 	        
 	        list.clear();
-	       // System.out.println("Clear");
-	        
 	        list.addAll(s);
-	       // System.out.println("New List: "+ list.addAll(s));
+	       
 
 	    }
 	
